406f46efca9d32ee0e9e7a6c2fe33371b6d04a31
KittenAvenger <nevadaa51@yahoo.se>
2015-05-31 21:16:52 +0200

    Update countrycode.erl

--- src/countrycode.erl
+++ src/countrycode.erl
@@ -1,11 +1,12 @@
 -module(countrycode).
-
 -export([start/1, retrieve_country_list/0]).
 
+%	Returns a list with country names and amount of players
+
 start (List) ->
-
-
 [{find(N), Players} || {N, Players} <- List].
+
+%	Search for country name from country code, if not in database search the countrylist, otherwise return the name
 
 find (CountryCode) ->
 	case CountryName = database:get_countrycode(CountryCode) of
@@ -13,8 +14,8 @@ find (CountryCode) ->
 		_ -> CountryName
 	end.
 	
-		 
-	
+%	One time function used to download the countrycode list with country names, only run once
+
 retrieve_country_list() ->
 
 ssl:start(),
@@ -24,10 +25,11 @@ application:start(inets),
 {struct, JsonData} = mochijson:decode(JSON),
 database:store_countrylist(JsonData).
 
+%	Parse country name from country list in case one couldn't be found in the database
+
 find_country_name(CountryCode) ->
 
 {_, Data1} = proplists:get_value(atom_to_list(CountryCode), database:get_countrylist()),
 Data2 = proplists:get_value("name",Data1),
 database:store_countrycode(CountryCode, Data2), 
 Data2.
-
